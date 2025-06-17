2e205812f4d9e3d175b409b8100cab4342a44575
Karthik M Sarma <karthikmsarma@gmail.com>
2025-06-18 04:57:52 +0530

    commit

--- app.py
+++ app.py
@@ -70,8 +70,8 @@ def send_message():
     })
 
     
-    
+import os
+
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port= 8080, debug= True)
-
-
+    port = int(os.environ.get('PORT', 8080))  
+    app.run(host='0.0.0.0', port=port, debug=True)
